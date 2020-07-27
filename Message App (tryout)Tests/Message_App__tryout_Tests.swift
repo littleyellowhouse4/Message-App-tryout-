362de2cbb82916a3ee7bad5f35278c75f38d41b7
Kailah Boston <kailahboston@gmail.com>
2020-07-27 12:10:38 -0400

    Initial Commit

---
+++ Message App (tryout)Tests/Message_App__tryout_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Message_App__tryout_Tests.swift
+//  Message App (tryout)Tests
+//
+//  Created by Apple on 7/27/20.
+//  Copyright © 2020 Kailah Boston. All rights reserved.
+//
+
+import XCTest
+@testable import Message_App__tryout_
+
+class Message_App__tryout_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
